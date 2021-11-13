979a21c697c868d61c5f6ec692338eac44f3da85
fusunnn <i.audouard22@ejm.org>
2021-11-13 03:10:06 +0100

    fixed api

--- frontend/src/logic/ApiHandler.ts
+++ frontend/src/logic/ApiHandler.ts
@@ -1,14 +1,18 @@
 function parseAnimeTitleForApi(animeTitle: string) {
   return animeTitle.replaceAll(" ", "-");
 }
-export default function fetchRecommendations(
+export default async function fetchRecommendations(
   animeTitle: string,
   numberOfRecommendations: number
 ) {
-  const baseUrl: string = "https://ivanadrd.pythonanywhere.com/api/";
-  fetch(
-    baseUrl + parseAnimeTitleForApi(animeTitle) + "+" + numberOfRecommendations
-  )
+  const url: string = `https://ivanadrd.pythonanywhere.com/api/${parseAnimeTitleForApi(
+    animeTitle
+  )}+${numberOfRecommendations}`;
+  // const url: string = `http://127.0.0.1:5000/api/${parseAnimeTitleForApi(
+  //   animeTitle
+  // )}+${numberOfRecommendations}`;
+
+  const data: any = await fetch(url)
     .then((res) => {
       return res.json();
     })
