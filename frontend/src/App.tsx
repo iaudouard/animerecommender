f8008124b7bf8d3f33c66ccb28c6d0c3932a4651
fusunnn <i.audouard22@ejm.org>
2021-11-13 04:35:51 +0100

    typescript implementation working logically, need to style main page and create results page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/App.css";
+import IncrementButton from "./components/IncrementButton";
 import fetchRecommendations from "./logic/ApiHandler";
 
 function App() {
-  fetchRecommendations("Naruto", 5);
+  const [animeSearchInput, setAnimeSearchInput] = useState<string>("");
+  const [numberOfRecommendations, setNumberOfRecommendations] =
+    useState<number>(0);
+
+  const updateAnimeSearchInput = (
+    ev: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setAnimeSearchInput(ev.target.value);
+  };
+
+  const handleIncrementDown = (): void => {
+    numberOfRecommendations > 0
+      ? setNumberOfRecommendations(numberOfRecommendations - 1)
+      : console.log("");
+  };
+
+  const handleIncrementUp = (): void => {
+    setNumberOfRecommendations(numberOfRecommendations + 1);
+  };
+
+  const handleSubmit = (): void => {
+    console.log(
+      fetchRecommendations(animeSearchInput, numberOfRecommendations)
+    );
+  };
+
   return (
     <div className="App">
-      <input></input>
+      <input onChange={(ev) => updateAnimeSearchInput(ev)}></input>
+      <div className="incrementContainer">
+        <IncrementButton
+          primaryColor="#ffaeb6"
+          secondaryColor="#a30000"
+          incrementFunction={() => handleIncrementDown()}
+          sign="-"
+        />
+        {numberOfRecommendations}
+        <IncrementButton
+          primaryColor="#ffaeb6"
+          secondaryColor="#a30000"
+          incrementFunction={() => handleIncrementUp()}
+          sign="+"
+        />
+      </div>
+      <button onClick={() => handleSubmit()}>Submit</button>
     </div>
   );
 }
--- frontend/src/components/IncrementButton.tsx
+++ frontend/src/components/IncrementButton.tsx
@@ -1,7 +1,26 @@
 import React, { ReactElement } from "react";
+import "../styles/IncrementButton.css";
 
-interface Props {}
+interface Props {
+  sign: string;
+  incrementFunction: Function;
+  primaryColor: string;
+  secondaryColor: string;
+}
 
-export default function IncrementButton({}: Props): ReactElement {
-  return <div></div>;
+export default function IncrementButton({
+  sign,
+  incrementFunction,
+  primaryColor,
+  secondaryColor,
+}: Props): ReactElement {
+  return (
+    <div
+      className="incrementButton"
+      onClick={() => incrementFunction()}
+      style={{ backgroundColor: primaryColor, color: secondaryColor }}
+    >
+      {sign}
+    </div>
+  );
 }
