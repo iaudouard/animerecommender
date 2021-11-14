042b278fb214ce8ab09733b6e005391cd028858a
fusunnn <i.audouard22@ejm.org>
2021-11-14 05:32:00 +0100

    adding files changed, finished styling home and added routing

---
+++ frontend/src/logic/api.ts
@@ -0,0 +1,16 @@
+export default async function fetchRecommendations(
+  animeTitle: string,
+  numberOfRecommendations: number
+) {
+  const url: string = `https://ivanadrd.pythonanywhere.com/?anime_title=${animeTitle}&number_of_anime=${numberOfRecommendations}`;
+
+  const data: any = await fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json.data);
+    });
+
+  return data;
+}
