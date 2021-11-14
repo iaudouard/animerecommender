0d14c3c6a5c46257ed93ccfa0c86eb48a9745a7e
fusunnn <i.audouard22@ejm.org>
2021-11-14 21:24:15 +0100

    feat: added two new themes and possibility to switch

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 import "../styles/Home.css";
 import fetchRecommendations from "../logic/api";
 import Button from "../components/Button";
+import ThemeButton from "../components/ThemeButton";
 import { Link } from "react-router-dom";
+import { darling, poop, hana } from "../constants/themes";
 
 interface Props {}
 
 const App = () => {
+  const themes = [poop, hana, darling];
+  const [themeIndex, setThemeIndex] = useState<number>(0);
+  const [searchInputFocus, setSearchInputFocus] = useState<boolean>(false);
+  const [colorTheme, setColorTheme] = useState(themes[themeIndex]);
   const [animeSearchInput, setAnimeSearchInput] = useState<string>("");
   const [numberOfRecommendations, setNumberOfRecommendations] =
     useState<number>(0);
@@ -33,40 +39,62 @@ const App = () => {
     );
   };
 
+  const handleThemeSwitch = () => {
+    let tempIndex = themeIndex;
+    let newIndex = tempIndex < themes.length - 1 ? tempIndex + 1 : 0;
+    setThemeIndex(newIndex);
+    setColorTheme(themes[newIndex]);
+  };
+
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundColor: colorTheme["bng"] }}>
+      <ThemeButton
+        Icon={colorTheme["Icon"]}
+        Color={colorTheme["primary"]}
+        clickHandler={() => handleThemeSwitch()}
+      />
       <div className="formContainer">
         <input
           className="animeSearchInput"
           onChange={(ev) => updateAnimeSearchInput(ev)}
           placeholder="enter an anime..."
+          style={{
+            borderBottom: `0.4vh solid ${
+              searchInputFocus ? "white" : colorTheme["secondary"]
+            }`,
+          }}
+          onFocus={() => setSearchInputFocus(true)}
+          onBlur={() => setSearchInputFocus(false)}
         ></input>
-        <div className="incrementContainer">
+        <div
+          className="incrementContainer"
+          style={{ color: colorTheme["secondary"] }}
+        >
           <Button
-            primaryColor="#ffaeb6"
-            secondaryColor="#a30000"
-            terceryColor="#f68a95"
+            primaryColor={colorTheme["primary"]}
+            secondaryColor={colorTheme["secondary"]}
+            terceryColor={colorTheme["tercery"]}
             handleClick={() => handleIncrementDown()}
             label="-"
             type="increment"
           />
           {numberOfRecommendations}
           <Button
-            primaryColor="#ffaeb6"
-            secondaryColor="#a30000"
-            terceryColor="#f68a95"
+            primaryColor={colorTheme["primary"]}
+            secondaryColor={colorTheme["secondary"]}
+            terceryColor={colorTheme["tercery"]}
             handleClick={() => handleIncrementUp()}
             label="+"
             type="increment"
           />
         </div>
       </div>
-      <Link to="/results">
+      <Link to="/results" style={{ textDecoration: "none" }}>
         <Button
           type="submit"
-          primaryColor="#ffaeb6"
-          secondaryColor="#a30000"
-          terceryColor="#f68a95"
+          primaryColor={colorTheme["primary"]}
+          secondaryColor={colorTheme["secondary"]}
+          terceryColor={colorTheme["tercery"]}
           handleClick={() => handleSubmit()}
           label="submit"
         />
