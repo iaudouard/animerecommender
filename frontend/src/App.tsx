887efd9d28013c41b182e2c79aca9daa5ad6e626
fusunnn <i.audouard22@ejm.org>
2021-11-15 18:07:12 +0100

    added global context to keep theme consistent even if user reloads, started making cards for results

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Results from "./pages/Results";
+import { themes } from "./constants/themes";
+import ThemeButton from "./components/ThemeButton";
+
+export const ThemeContext = createContext(themes[0]);
 
 interface Props {}
 
 export default function App({}: Props) {
+  const [colorTheme, setColorTheme] = useState<any>(themes[0]);
+
+  const handleThemeSwitch = () => {
+    let tempIndex = themes.indexOf(colorTheme);
+    let newIndex = tempIndex < themes.length - 1 ? tempIndex + 1 : 0;
+    setColorTheme(themes[newIndex]);
+  };
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/results" exact component={Results} />
-      </Switch>
-    </Router>
+    <ThemeContext.Provider value={colorTheme}>
+      <ThemeButton
+        Icon={colorTheme["Icon"]}
+        Color={colorTheme["primary"]}
+        clickHandler={() => handleThemeSwitch()}
+      />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/results" exact component={Results} />
+        </Switch>
+      </Router>
+    </ThemeContext.Provider>
   );
 }
