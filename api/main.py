e6832f77bf1c441d2fc39e3c22239de66e3a352d
fusunnn <i.audouard22@ejm.org>
2021-11-23 20:03:36 +0100

    added a file to write and see what people are searching

--- api/main.py
+++ api/main.py
@@ -9,21 +9,23 @@ animes = openAnimes()
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    anime_title =  request.args.get('anime_title')
-    number_of_anime =  request.args.get('number_of_anime')
+    anime_title = request.args.get('anime_title')
+    number_of_anime = request.args.get('number_of_anime')
 
-    print(anime_title, number_of_anime)
+    write_input(anime_title, number_of_anime)
+
     if anime_title in animes:
-        return jsonify({'data' : (run(animes, anime_title, int(number_of_anime)))})
+        return jsonify({'data': (run(animes, anime_title, int(number_of_anime)))})
     else:
-        return jsonify({'data' : f"Anime Not Found"})
+        return jsonify({'data': f"Anime Not Found"})
+
 
 @app.route("/get_titles", methods=['GET', 'POST'])
 def get_animes():
 
     anime_titles = getTitles()
 
-    return jsonify({'data' : anime_titles})
+    return jsonify({'data': anime_titles})
 
 
 @app.route("/get_anime_info", methods=['GET', 'POST'])
@@ -31,26 +33,19 @@ def get_anime_info():
 
     anime = request.args.get('anime_title')
     if anime in animes:
-        return jsonify({'data' : animes[anime]})
+        return jsonify({'data': animes[anime]})
     else:
-        return jsonify({'data' : f"Anime Not Found"})
+        return jsonify({'data': f"Anime Not Found"})
 
 
 @app.route("/title_exists", methods=['GET', 'POST'])
 def title_exists():
     anime_title = request.args.get('anime_title')
     if anime_title in animes:
-        return jsonify({"data" : True})
+        return jsonify({"data": True})
     else:
         return jsonify({"data": False})
 
+
 if __name__ == '__main__':
-	app.run(debug=True)
-
-
-
-
-
-
-
-
+    app.run(debug=True)
