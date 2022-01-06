756d475db6a6cc0f857b00d319be82d6afad45ce
fusunnn <i.audouard22@ejm.org>
2022-01-06 18:38:52 +0100

    feat: api logic update

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,44 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosResponse, AxiosRequestConfig } from "axios";
 
-export default async function fetch(url: string) {
+export async function fetchRec(animeTitle: string, numberOfAnime: string) {
+  const options: AxiosRequestConfig = {
+    method: "GET",
+    url: "https://anime-recommender.p.rapidapi.com/",
+    params: { anime_title: animeTitle, number_of_anime: numberOfAnime },
+    headers: {
+      "x-rapidapi-host": process.env.REACT_APP_RAPID_HOST!,
+      "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY!,
+    },
+  };
+
+  const data = await axios.request(options).then((response: AxiosResponse) => {
+    return response.data;
+  });
+  return data;
+}
+
+export async function fetchAutocomplete(text: string) {
+  const url = `https://kitsu.io/api/edge/anime?filter[text]=${text}`;
+
   const data = await axios.get(url).then((response: AxiosResponse) => {
+    return response.data;
+  });
+
+  return data;
+}
+
+export async function fetchInfo(animeTitle: string) {
+  const options: AxiosRequestConfig = {
+    method: "GET",
+    url: "https://anime-recommender.p.rapidapi.com/get_anime_info",
+    params: { anime_title: animeTitle },
+    headers: {
+      "x-rapidapi-host": process.env.REACT_APP_RAPID_HOST!,
+      "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY!,
+    },
+  };
+
+  const data = await axios.request(options).then((response: AxiosResponse) => {
     return response.data;
   });
   return data;
