95435ad806b2c339a7002bb9ac9872bf8e2e759c
Ernesto <ernestodemenibus@gmail.com>
2022-02-04 00:52:29 +0100

    finished autocomplete + new btter rec engine

--- api/auto_complete.py
+++ api/auto_complete.py
@@ -1,14 +1,13 @@
-from turtle import title
+from typing import final
 from fast_autocomplete import autocomplete_factory
 import json
-import logic
 from fast_autocomplete import AutoComplete
+#import logic
 
-"""
-animes = logic.openAnimes()
+"""animes = logic.openAnimes()
 
 ani_titles = {x.lower() : {} for x in animes}
-check = {x.lower() : x for x in animes}
+check = {x.lower() : {"title":x, "rating":animes[x]["seasonYear"]} for x in animes}
 
 for x in animes:
      
@@ -16,21 +15,20 @@ for x in animes:
     for y in titles:
         if titles[y] is not None:
             ani_titles[titles[y].lower()] = {}
-            check[titles[y].lower()] = x
+            check[titles[y].lower()] = {"title":x, "rating":animes[x]["seasonYear"]}
     
             
 
     titles = animes[x]["synonyms"]
     for y in titles:
         ani_titles[y.lower()] = {}
-        check[y.lower()] = x
+        check[y.lower()] = {"title":x, "rating":animes[x]["seasonYear"]}
 
     
 with open("anime_titles.json", "w") as ft:
     json.dump([ani_titles, check], ft )
 
 """
-
 
 """content_files = {
     'words': {
@@ -49,7 +47,6 @@ for x in words:
 
 """
 
-
 with open("anime_titles.json", "r") as ft:
     ani_titles, check = list(json.load(ft))
 
@@ -61,22 +58,37 @@ print("tree made")
 def auto_comp(search, amnt):
 
     search = search.replace(" ", "")
-    word_search = slower_auto.search(word=search, max_cost=8, size=amnt)
+    word_search = slower_auto.search(word=search, max_cost=5, size=5)
 
     results = []
 
     for x in word_search:
         for y in x:
-            results.append(check[y])
+            rating = check[y]["rating"]
+            if rating is None:
 
-    return results
+                results.append((check[y]["title"], 10000))
+            else:
+                results.append((check[y]["title"], rating))
+
+
+    results.sort(key = lambda x:x[1])
+    return [x[0] for x in results]
 
 
 
+"""
+with open("data/anime_db.json") as f:
+    animes = json.load(f)
 
-while True:
+for x in animes:
+    eng_tit = animes[x]["title"]["english"]
 
-    search = input("Search :")
-
-    print(auto_comp(search, 5))
+    if eng_tit is not None:
+        animes[x]["title"]["userPreferred"] = eng_tit
     
+
+
+with open("data/anime_db.json", "w") as f:
+    json.dump(animes, f)
+"""
