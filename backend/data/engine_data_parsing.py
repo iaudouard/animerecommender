cfa3417f39195e898a5cb7c3585b172e20daa3f6
Ernesto <E.L.M.HellouindeMenibus@student.tudelft.nl>
2023-04-24 09:15:08 +0200

    data

---
+++ backend/data/engine_data_parsing.py
@@ -0,0 +1,35 @@
+import json
+
+DB_PATH = "./json/anime_db.json"
+
+
+def open_json(filepath: str):
+    with open(filepath, "r") as f:
+        return json.load(f)
+
+
+def save_json(filepath: str, data: any):
+    with open(filepath, "w") as f:
+        json.dump(data, f)
+
+
+def main(db_path: str):
+    # TODO : clean up data so there are no season 2 of things etc
+    data = open_json(db_path)
+    updated_data = []
+    for anime in data:
+        temp = []
+        for info in data[anime]:
+            if info in ["id", "seasonYear", "type", "format", "averageScore", "tag_vec"]:
+                temp.append(data[anime][info])
+
+            if info == "gen_vec":
+                temp.append(data[anime][info])
+                temp.append(int(''.join(str(i) for i in data[anime][info]), 2))
+
+        updated_data.append(tuple(temp))
+
+    save_json("./json/data_as_list.json", updated_data)
+
+
+main(DB_PATH)
