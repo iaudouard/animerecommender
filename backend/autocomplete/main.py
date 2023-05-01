6de256eb7a4bcc35cd4f5f001fb9f89d15e60aa3
Ivan Audouard <i.audouard22@ejm.org>
2023-04-30 21:39:35 -0400

    refactored response structure

--- backend/autocomplete/main.py
+++ backend/autocomplete/main.py
@@ -11,4 +11,4 @@ autocomplete = AnimeAutocomplete(filepath="data/anime_titles.json")
 @app.route("/autocomplete/{query}", methods=["GET"])
 def get_autocomplete(query: str):
     results = autocomplete.search(query)
-    return jsonify(results)
+    return jsonify(results=results)
