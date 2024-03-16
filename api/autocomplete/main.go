59bcf2b7a82c0ea7776684237dc88f0bb62c7585
Ivan Audouard <i.audouard22@ejm.org>
2024-03-16 01:20:57 +0000

    feat: added passing arg for testing

--- api/autocomplete/main.go
+++ api/autocomplete/main.go
@@ -2,11 +2,20 @@ package main
 
 import (
   "log"
+  "flag"
 )
 
 func main() {
-  res := getAutocomplete("shingeki no kyojin")
-  log.Printf("results: ", res)
-  res = getAutocomplete("naruto")
+  var input string
+  flag.StringVar(&input, "anime", "", "input to get autocomplete for")
+  flag.Parse()
+
+  log.Printf("building autocomplete for: ", input, "...")
+
+  if input == "" {
+    log.Fatalf("please provide an input for arg 'anime'")
+  }
+
+  res := getAutocomplete(input)
   log.Printf("results: ", res)
 }
